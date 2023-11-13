23637762d165907d23012462cd970efb0c7bf320
GlebKostenko <glebkostenko35@gmail.com>
2023-11-13 13:30:21 +0300

    task 6

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class Main {
-    public static void main(String[] args) {
-       String str = "Hello World!";
-        System.out.println(str.charAt(1));
-        Object obj = new Object();
+    public static void main(String[] args) throws ParseException {
+        LocalDate date = LocalDate.parse("07/14/2015", DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+        System.out.println(date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.CANADA)));
     }
 }
